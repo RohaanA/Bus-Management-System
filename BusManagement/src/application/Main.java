87ddb9e0ce8985497269ecf1fd4d1992a45133ba
Rohaan Atique <honeyr697@gmail.com>
2022-11-05 23:56:20 +0500

    Added comment

--- BusManagement/src/application/Main.java
+++ BusManagement/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-
+			//Testing
 			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
