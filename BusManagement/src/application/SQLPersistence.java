c7ce288f52e86a31d2ea0850dfb35b190374391a
Ahmed-M1 <88033251+Ahmed-M1@users.noreply.github.com>
2022-11-11 23:52:56 +0500

    SQL PERSISTENCE

---
+++ BusManagement/src/application/SQLPersistence.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.sql.*;
+
+//Database persistence Handler
+
+
+public class SQLPersistence {
+	
+	public void authenticate_Manager(String user,String pass) throws ClassNotFoundException, SQLException {
+		
+		
+		System.out.println("Worked Till here");
+		int found=0;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection con=DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/BusManagement","root","moizrules1");
+		
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery("SELECT COUNT(*) FROM Manager WHERE Username='"+ user +"' AND Password='"+pass+ "'; ");
+		
+		while(rs.next())
+		{
+			found=rs.getInt("COUNT(*)");
+		}
+		
+		if(found==0)
+		{
+			System.out.println("Incorrect Username/Password");
+		}
+		
+		else
+		{
+			System.out.println("Logged In");
+		}
+	}
+	
+
+}
