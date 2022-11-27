62d2293018630d0dcc9dbae628e0a8f858d89b66
M. Rohaan Atique <i200410@nu.edu.pk>
2022-11-27 18:46:04 +0500

    fixin

--- BusManagement/src/db/PersistenceHandler.java
+++ BusManagement/src/db/PersistenceHandler.java
@@ -22,4 +22,6 @@ public abstract class PersistenceHandler {
 	public abstract ArrayList<String> getAllRouteLocations() throws SQLException;
 	public abstract ResultSet getAllRouteData() throws SQLException;
 	public abstract boolean changeBookingStatus(int routeID);
+	public abstract ArrayList<Integer> getBookedSeats(int routeID) throws SQLException;
+	public abstract int getBusSeatCount(String busID) throws SQLException;
 }
